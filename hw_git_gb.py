0f0476d08b91304dc4844c5db30fdaeb2dce2dc2
vicsergev <v.s.sergevnin@gmail.com>
2018-11-23 12:57:06 +0300

    extended output

--- hw_git_gb.py
+++ hw_git_gb.py
@@ -2,4 +2,8 @@ import random
 
 choice_list = input('Введите список ').split()
 
-print('Случайный элемент:', random.choice(choice_list))
+print('Выбираем из: ')
+for elem in choice_list:
+    print(elem, end=', ')
+
+print('\nСлучайный элемент:', random.choice(choice_list))
